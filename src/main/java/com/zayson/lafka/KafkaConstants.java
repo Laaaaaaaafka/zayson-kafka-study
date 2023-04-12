da43ce8b2edd4f8de6776d8a760dc6d406f6d903
zayson <jymaeng95@gmail.com>
2023-04-13 00:42:34 +0900

    feat: Kafka Constant 추가

---
+++ src/main/java/com/zayson/lafka/KafkaConstants.java
@@ -0,0 +1,8 @@
+package com.zayson.lafka;
+
+public final class KafkaConstants {
+    public static final String KAFKA1 = "kafka1:9092";
+    public static final String KAFKA2 = "kafka2:9092";
+    public static final String KAFKA3 = "kafka3:9092";
+
+}

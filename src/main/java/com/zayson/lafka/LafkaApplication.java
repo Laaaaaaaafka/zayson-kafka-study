94f813be770f0788beaa43c103ddba6e3fffe098
zayson <jymaeng95@gmail.com>
2023-04-12 23:21:11 +0900

    setting: Initial Commit

---
+++ src/main/java/com/zayson/lafka/LafkaApplication.java
@@ -0,0 +1,13 @@
+package com.zayson.lafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zayson/lafka/LafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zayson.lafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

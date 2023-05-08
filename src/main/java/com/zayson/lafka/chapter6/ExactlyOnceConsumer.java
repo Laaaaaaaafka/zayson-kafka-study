42915568757a5646888991db2db68a7f89a171d2
zayson <jymaeng95@gmail.com>
2023-05-08 16:00:33 +0900

    feat: Exactly Once Consumer 예제 코드 추가

---
+++ src/main/java/com/zayson/lafka/chapter6/ExactlyOnceConsumer.java
@@ -0,0 +1,44 @@
+package com.zayson.lafka.chapter6;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ExactlyOnceConsumer {
+    public static void main(String[] args) {
+        String bootstrapServers = "kafka1:9092";
+        Properties props = new Properties();
+
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "peter-consumer-01");
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.setProperty(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");  // 정확히 한 번 전송을 위한 설정
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("peter-test05"));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(1000);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("Topic : %s, Partition: %s, Offset %d, Key: %s, value:%s\n",
+                            record.topic(), record.partition(), record.offset(), record.key(), record.value());
+                }
+                consumer.commitAsync();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            consumer.close();
+        }
+    }
+}
